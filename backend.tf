6b7b3a927c4929f7c9fc13fe286f551f8cb7b012
i3oi3ka <i3oi3ka@ukr.net>
2026-04-12 00:07:35 +0300

    Update Terraform configuration for lesson-5: modify S3 bucket name, adjust AWS region, and enhance VPC module with public/private subnets and routing

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "ruday-lesson-5-terraform-state-bucket-1"
+    bucket         = "ruday-lesson-5-terraform-state-bucket-5"
     key            = "lesson-5/terraform.tfstate"   # Шлях до файлу стейту
-    region         = "us-west-2"                    # Регіон AWS
+    region         = "eu-west-2"                    # Регіон AWS
     dynamodb_table = "ruday-lesson-5-terraform-locks"             # Назва таблиці DynamoDB
     encrypt        = true                           # Шифрування файлу стейту
   }
--- main.tf
+++ main.tf
@@ -1,6 +1,23 @@
 # Підключаємо модуль для S3 та DynamoDB
 module "s3_backend" {
   source = "./modules/s3-backend"                # Шлях до модуля
-  bucket_name = "ruday-lesson-5-terraform-state-bucket-1"  # Ім'я S3-бакета
+  bucket_name = "ruday-lesson-5-terraform-state-bucket-5"  # Ім'я S3-бакета
   table_name  = "ruday-lesson-5-terraform-locks"                # Ім'я DynamoDB
 }
+
+# Підключаємо модуль для VPC
+module "vpc" {
+  source              = "./modules/vpc"           # Шлях до модуля VPC
+  vpc_cidr_block      = "10.0.0.0/16"             # CIDR блок для VPC
+  public_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]        # Публічні підмережі
+  private_subnets     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]         # Приватні підмережі
+  availability_zones  = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]            # Зони доступності
+  vpc_name            = "vpc"              # Ім'я VPC
+}
+
+# Підключаємо модуль ECR
+# module "ecr" {
+#   source      = "./modules/ecr"
+#   ecr_name    = "lesson-5-ecr"
+#   scan_on_push = true
+# }
